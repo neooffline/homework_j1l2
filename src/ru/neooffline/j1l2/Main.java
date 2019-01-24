0355dedeee3f625e88cecd5b839ae1e661d77487
neooffline <admin@neooffline.ru>
2019-01-24 23:01:15 +0300

    задание 5

--- src/ru/neooffline/j1l2/Main.java
+++ src/ru/neooffline/j1l2/Main.java
@@ -3,6 +3,30 @@ package ru.neooffline.j1l2;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        double[] arrayTest = {-100,4242,4242.1,-1,2,3,5,10,-20,18};
+        printArray(findMinMaxElement(arrayTest));
+    }
+    //Задание 5
+    static double[] findMinMaxElement(double[] array){
+        double[] arrResult = new double[2];
+        for (int i = 0; i < array.length; i++) {
+            if(arrResult[0]>array[i]){
+                arrResult[0]=array[i];
+            }
+            if (arrResult[1]<array[i]){
+                arrResult[1]=array[i];
+            }
+        }
+        return arrResult;
+    }
+    static void printArray(double[] array){
+        for (double value:array
+             )
+        if ((int)value==value){
+            System.out.println((int) value);
+        } else {
+            System.out.println(value);
+        }
     }
 }
